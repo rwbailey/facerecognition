2bafe6205cf339db224083c3e5ba67a8951bff22
Richard Bailey <richard@richardbailey.xyz>
2020-05-16 00:47:47 +0100

    Added particles

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import './App.css';
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particleOptions}
+      />
       <Navigation />
       <Logo />
       <Rank />  
