453213182e6a0bbefc981506c9f507c97a505c59
Richard Bailey <richard@richardbailey.xyz>
2020-05-17 22:34:35 +0100

    Signout route

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -70,6 +71,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false});
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true});
+    }
     this.setState({route: route});
   }
 
@@ -79,7 +85,7 @@ class App extends Component {
         <Particles className='particles'
           params={particleOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn} />
         { this.state.route === 'home'
           ? <>
               <Logo />
